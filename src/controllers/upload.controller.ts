ceab58c5ef4986401a224b5b9ce23ee47102d0b6
DanielFernandoYepezVelez <danipez.02@gmail.com>
2020-08-11 10:38:32 -0500

    Actualizando Controladores De Imagenes Y De Usuarios

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -11,96 +11,94 @@ import { unaSolaImgDB } from '../utils/helpers/img/unaSolaImgDB';
 import { userFind } from '../utils/helpers/user/userFind';
 
 class UploadController {
-  async fileUpload(req: Request, res: Response): Promise<any> {
-    const { tipo, id } = req.params;
-    const tiposValidos = ['users', 'doctors', 'hospitals'];
+ async fileUpload(req: Request, res: Response): Promise<any> {
+  const { tipo, id } = req.params;
+  const tiposValidos = ['users', 'doctors', 'hospitals'];
 
-    try {
-      if (!tiposValidos.includes(tipo)) {
-        throw new Error('No es un usuario, doctor u hospital').message;
-      }
+  try {
+   if (!tiposValidos.includes(tipo)) {
+    throw new Error('No es un usuario, doctor u hospital').message;
+   }
 
-      if (!req.files || Object.keys(req.files).length === 0) {
-        throw new Error('No Hay Ningun Archivo').message;
-      }
+   if (!req.files || Object.keys(req.files).length === 0) {
+    throw new Error('No Hay Ningun Archivo').message;
+   }
 
-      /* Validar ID Antes De Mover Imagenes Al Servidor */
-      const query_u = pool.query('SELECT id FROM users WHERE id = ?', [id]);
-      const query_d = pool.query('SELECT id FROM doctors WHERE id = ?', [id]);
-      const query_h = pool.query('SELECT id FROM hospitals WHERE id = ?', [id]);
+   /* Validar ID Antes De Mover Imagenes Al Servidor */
+   const query_u = pool.query('SELECT id FROM users WHERE id = ?', [id]);
+   const query_d = pool.query('SELECT id FROM doctors WHERE id = ?', [id]);
+   const query_h = pool.query('SELECT id FROM hospitals WHERE id = ?', [id]);
 
-      const [user, doctor, hospital] = await Promise.all([
-        query_u,
-        query_d,
-        query_h,
-      ]);
+   const [user, doctor, hospital] = await Promise.all([
+    query_u,
+    query_d,
+    query_h,
+   ]);
 
-      if (tipo === 'users') {
-        userFind.findUser(user[0]);
-      } else if (tipo === 'doctors') {
-        userFind.findUser(doctor[0]);
-      } else if (tipo === 'hospitals') {
-        userFind.findUser(hospital[0]);
-      }
+   if (tipo === 'users') {
+    userFind.findUser(user[0]);
+   } else if (tipo === 'doctors') {
+    userFind.findUser(doctor[0]);
+   } else if (tipo === 'hospitals') {
+    userFind.findUser(hospital[0]);
+   }
 
-      /* Procesar la imagen */
-      const file: any = req.files.imagen;
-      const name: string[] = file.name.split('.');
-      const ext: string = name[name.length - 1];
+   /* Procesar la imagen */
+   const file: any = req.files.imagen;
+   const name: string[] = file.name.split('.');
+   const ext: string = name[name.length - 1];
 
-      /* Validar Extensión */
-      const extValidas = ['png', 'jpg', 'jpge', 'gif'];
-      if (!extValidas.includes(ext)) {
-        throw new Error('Extension No Valida').message;
-      }
+   /* Validar Extensión */
+   const extValidas = ['png', 'jpg', 'jpge', 'gif'];
+   if (!extValidas.includes(ext)) {
+    throw new Error('Extension No Valida').message;
+   }
 
-      /* Generar el nombre del archivo de la imagen */
-      let nombreArchivo = `${uuidv4()}.${ext}`;
+   /* Generar el nombre del archivo de la imagen */
+   let nombreArchivo = `${uuidv4()}.${ext}`;
 
-      /* Path Para Guardar La Imagen En El Servidor */
-      const pathFile = path.resolve(
-        __dirname,
-        `../assets/uploads/img/${tipo}/${nombreArchivo}`
-      );
+   /* Path Para Guardar La Imagen En El Servidor */
+   const pathFile = path.resolve(
+    __dirname,
+    `../assets/uploads/img/${tipo}/${nombreArchivo}`
+   );
 
-      /* Ojo Con Esto Por Que Es Un CallBack No Se Aceptan Promesas(OJO!!) */
-      await file.mv(pathFile);
+   /* Ojo Con Esto Por Que Es Un CallBack No Se Aceptan Promesas(OJO!!) */
+   await file.mv(pathFile);
 
-      /* Actualización En La Base De Datos */
-      const ok = await unaSolaImgDB.saveImage(tipo, id, nombreArchivo);
+   /* Actualización En La Base De Datos */
+   const ok = await unaSolaImgDB.saveImage(tipo, id, nombreArchivo);
 
-      return res.json({
-        ok,
-        mgs: 'File Uploaded Successfully',
-      });
-    } catch (e) {
-      return res.status(400).json({
-        ok: false,
-        error: e,
-        mgs: 'File NOT Uploaded Successfully',
-      });
-    }
+   return res.json({
+    ok,
+    nombreArchivo,
+    mgs: 'File Uploaded Successfully',
+   });
+  } catch (e) {
+   return res.status(400).json({
+    ok: false,
+    error: e,
+    mgs: 'File NOT Uploaded Successfully',
+   });
   }
+ }
 
-  returnImage(req: Request, res: Response) {
-    const { tipo, image } = req.params;
+ returnImage(req: Request, res: Response) {
+  const { tipo, image } = req.params;
 
-    const pathImage = path.join(
-      __dirname,
-      `../assets/uploads/img/${tipo}/${image}`
-    );
+  const pathImage = path.join(
+   __dirname,
+   `../assets/uploads/img/${tipo}/${image}`
+  );
 
-    if (fs.existsSync(pathImage)) {
-      res.sendFile(pathImage);
-    } else {
-      const pathImage = path.join(
-        __dirname,
-        `../assets/uploads/img/notfound.png`
-      );
+  if (fs.existsSync(pathImage)) {
+   res.sendFile(pathImage);
+  } else {
+   const pathImage = path.join(__dirname, `../assets/uploads/img/notfound.png`);
 
-      res.sendFile(pathImage);
-    }
+   res.sendFile(pathImage);
   }
+ }
 }
 
 export const uploadController = new UploadController();
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -13,136 +13,145 @@ import { getDataDB } from '../utils/helpers/getDataDB';
 import { IUser } from '../utils/models/IUser';
 
 class UserController {
-  /* PAGINACIÓN SQL(IMPORTANTE) */
-  async getUsers(req: Request, res: Response): Promise<Response<JSON>> {
-    let desde = Number(req.query.desde || 0);
+ /* PAGINACIÓN SQL(IMPORTANTE) */
+ async getUsers(req: Request, res: Response): Promise<Response<JSON>> {
+  let desde = Number(req.query.desde || 0);
 
-    try {
-      /* Consultas Lanzadas Al Mismo Tiempo(NodeJS Es Non-Blocking) */
-      const queryPag = pool.query(
-        'SELECT id, name, img, email, google, role, activate FROM users LIMIT 5 OFFSET ?',
-        [desde]
-      );
-      const queryTotal = pool.query(
-        'SELECT COUNT(DISTINCT id) AS Total FROM users'
-      );
+  try {
+   /* Consultas Lanzadas Al Mismo Tiempo(NodeJS Es Non-Blocking) */
+   const queryPag = pool.query(
+    'SELECT id, name, img, email, google, role, activate FROM users LIMIT 5 OFFSET ?',
+    [desde]
+   );
+   const queryTotal = pool.query(
+    'SELECT COUNT(DISTINCT id) AS Total FROM users'
+   );
 
-      const [users, totalUsers] = await Promise.all([queryPag, queryTotal]);
+   const [users, totalUsers] = await Promise.all([queryPag, queryTotal]);
 
-      return res.json({
-        ok: true,
-        users: users[0],
-        totalUsers: totalUsers[0],
-      });
-    } catch (e) {
-      return res.status(400).json({
-        ok: false,
-        error: e,
-      });
-    }
+   return res.json({
+    ok: true,
+    users: users[0],
+    totalUsers: totalUsers[0],
+   });
+  } catch (e) {
+   return res.status(400).json({
+    ok: false,
+    error: e,
+   });
   }
+ }
 
-  async createUser(req: Request, res: Response): Promise<Response<JSON>> {
-    const newUser: IUser = req.body;
+ async createUser(req: Request, res: Response): Promise<Response<JSON>> {
+  const newUser: IUser = req.body;
 
-    try {
-      const query = await pool.query(
-        'SELECT email FROM users WHERE email = ?',
-        [newUser.email]
-      );
+  try {
+   const query = await pool.query('SELECT email FROM users WHERE email = ?', [
+    newUser.email,
+   ]);
 
-      /* Validate Email Unique */
-      emailFind.userEmailFind(query[0]);
+   /* Validate Email Unique */
+   emailFind.userEmailFind(query[0]);
 
-      /* Hash Password */
-      const hashPassword = bcrypt.hashPassword(newUser.password);
-      newUser.password = hashPassword;
+   /* Hash Password */
+   const hashPassword = bcrypt.hashPassword(newUser.password);
+   newUser.password = hashPassword;
 
-      await pool.query('INSERT INTO users SET ?', [newUser]);
+   await pool.query('INSERT INTO users SET ?', [newUser]);
 
-      return res.json({
-        ok: true,
-        message: 'User Created Successfully',
-      });
-    } catch (e) {
-      return res.status(400).json({
-        ok: false,
-        msg: 'User Not Created Successfully',
-        error: e,
-      });
-    }
+   return res.json({
+    ok: true,
+    message: 'User Created Successfully',
+   });
+  } catch (e) {
+   return res.status(400).json({
+    ok: false,
+    msg: 'User Not Created Successfully',
+    error: e,
+   });
   }
+ }
 
-  async updateUser(req: Request, res: Response): Promise<Response<JSON>> {
-    const { user_id } = req.params;
-    const { password, google, ...updateUser }: IUser = req.body;
+ async updateUser(req: Request, res: Response): Promise<Response<JSON>> {
+  const { user_id } = req.params;
+  const { password, google, ...updateUser }: IUser = req.body;
 
-    try {
-      let query = await pool.query('SELECT email FROM users WHERE id = ?', [
-        user_id,
-      ]);
+  try {
+   const googleQ = await pool.query('SELECT google FROM users WHERE id = ?', [
+    user_id,
+   ]);
 
-      /* Validar Existencia User */
-      userFind.findUser(query[0]);
+   const googleDB = getDataDB.init(googleQ[0], 2, 11);
 
-      /* Obtener Email-User-DB */
-      const emailDB = getDataDB.init(query[0], 3, 11);
+   if (googleDB !== '0') {
+    return res.status(400).json({
+     ok: false,
+     msg: 'User Is Google, No Update Successfully',
+    });
+   }
 
-      if (emailDB !== updateUser.email) {
-        query = await pool.query('SELECT email FROM users WHERE email = ?', [
-          updateUser.email,
-        ]);
+   let query = await pool.query('SELECT email FROM users WHERE id = ?', [
+    user_id,
+   ]);
 
-        /* Validar Email Entrante Unico */
-        emailFind.userEmailFind(query[0]);
-      }
+   /* Validar Existencia User */
+   userFind.findUser(query[0]);
 
-      query = await pool.query('UPDATE users SET ? WHERE id = ?', [
+   /* Obtener Email-User-DB */
+   const emailDB = getDataDB.init(query[0], 3, 11);
+
+   if (emailDB !== updateUser.email) {
+    query = await pool.query('SELECT email FROM users WHERE email = ?', [
+     updateUser.email,
+    ]);
+
+    /* Validar Email Entrante Unico */
+    emailFind.userEmailFind(query[0]);
+   }
+
+   query = await pool.query('UPDATE users SET ? WHERE id = ?', [
         updateUser,
         user_id,
       ]);
 
-      return res.json({
-        ok: true,
-        message: 'User Updated Successfully',
-      });
-    } catch (e) {
-      return res.status(400).json({
-        ok: false,
-        msg: 'User Not Updated Successfully',
-        error: e,
-      });
-    }
+   return res.json({
+    ok: true,
+    message: 'User Updated Successfully',
+   });
+  } catch (e) {
+   return res.status(400).json({
+    ok: false,
+    msg: 'User Not Updated Successfully',
+    error: e,
+   });
   }
+ }
 
-  async deleteUser(req: Request, res: Response): Promise<Response<JSON>> {
-    const { user_id } = req.params;
+ async deleteUser(req: Request, res: Response): Promise<Response<JSON>> {
+  const { user_id } = req.params;
 
-    try {
-      let query = await pool.query('SELECT id FROM users WHERE id = ?', [
-        user_id,
-      ]);
+  try {
+   let query = await pool.query('SELECT id FROM users WHERE id = ?', [user_id]);
 
-      /* Validar Existencia User */
-      userFind.findUser(query[0]);
+   /* Validar Existencia User */
+   userFind.findUser(query[0]);
 
-      query = await pool.query(
-        'UPDATE users SET activate = FALSE WHERE id = ?',
-        [user_id]
-      );
+   query = await pool.query('UPDATE users SET activate = FALSE WHERE id = ?', [
+    user_id,
+   ]);
 
-      return res.status(400).json({
-        ok: true,
-        msg: 'User Deleted Successfully',
-      });
-    } catch (e) {
-      return res.status(400).json({
-        ok: false,
-        msg: 'User Not Deleted Successfully',
-        error: e,
-      });
-    }
+   return res.status(400).json({
+    ok: true,
+    msg: 'User Deleted Successfully',
+   });
+  } catch (e) {
+   return res.status(400).json({
+    ok: false,
+    msg: 'User Not Deleted Successfully',
+    error: e,
+   });
   }
+ }
 }
 
 export const userController = new UserController();
